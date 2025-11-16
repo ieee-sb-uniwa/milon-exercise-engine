23c7827cb853552838f5448038e06682c8c6b557
Stelios Georgaras <steliosgp13@gmail.com>
2025-11-16 12:53:16 +0200

    delete old structure

--- src/pose/tracker.py
+++
@@ -1,8 +0,0 @@
-"""Multi-person pose tracking across video frames"""
-class PoseTracker:
-    def __init__(self):
-        self.tracks = {}
-    
-    def update(self, detections):
-        """Update tracks with new detections"""
-        pass
